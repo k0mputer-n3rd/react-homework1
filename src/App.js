bfa90611d21284f10e5e9dc8c68198eeb311b0b3
k0mputer n3rd <k0mputer-n3rd@outlook.com>
2022-04-16 20:58:10 -0700

    Day 01 Take-Home completed.

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import {MyFunction} from './utils';
+
+let appKey = "";
+
+function App() {
+  return (
+    <div>
+      Hello BitWise World!
+     
+    </div>
+  );
+}
+
+export default App;
+
+
+// const MyFunction = () => {
+//   let something = prompt("Enter Test Data");
+//   console.log(`The message is: ${something}`);
+//   return(
+//     <div>
+//         <h1>This is a test of {something}</h1>
+//     </div>
+//   )
+// }
+
---
+++ src/utils.js
@@ -0,0 +1,61 @@
+// utils.js
+
+// Function to respond to information typed in input box
+// export const myFunction = () => {
+//   let something = prompt("Enter Test Data");
+//   console.log(`The message is: ${something}`);
+//   return `The message is: ${something}`;
+// }
+
+export const MyFunction = () => {
+  if (!appKey) {
+    appKey = prompt("Enter App Key for OMDbAPI");
+  }
+  movieID = prompt("Enter Movie ID");
+  let something = getMoviesById(movieID);
+  console.log(`Returned value: ${something}`);
+  return(
+    <div>
+        <h1>You typed: {movieID}</h1>
+    </div>
+  )
+}
+
+// Function to search for movies by partial title
+export const searchMoviesByName = async (search) => {
+  let apiKey = prompt("Enter API Key");
+  // accessing the API
+  const url = `http://www.omdbapi.com/?apikey=${apiKey}&s=${search}`;
+  // Get an object of detailed information about the specified movie
+  const response = await fetch(url);
+  // return an array of movies with a matching title from OMDb API
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
+
+// Function to get movie information for a specific title
+export const getMovieByTitle = async (title) => {
+  let apiKey = prompt("Enter API Key");
+  // accessing the API
+  const url = `http://www.omdbapi.com/?apikey=${apiKey}&t=${title}`;
+  // Get an object of detailed information about the specified movie
+  const response = await fetch(url);
+  // return an array of movies with a matching title from OMDb API
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
+
+// Function to get movie information by movie ID
+export const getMoviesById = async (id) => {
+  let apiKey = prompt("Enter API Key");
+  // accessing the API
+  const url = `http://www.omdbapi.com/?apikey=${apiKey}&i=${id}`;
+  // Get an array of movies with a matching id from OMDb API
+  const response = await fetch(url);
+  // get an object of detailed information about the specified movie
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
